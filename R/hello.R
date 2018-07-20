0ba5cc4c422a2c3d101b77a93687f05b18cf7d27
gonzamaxilopez <gonzalo_lopez89@hotmail.com>
2018-07-20 11:31:04 -0300

    g

---
+++ R/hello.R
@@ -0,0 +1,29 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+#--------------------------------
+#Para verificar que todo esta bien copiar
+
+#Sys.getenv("PATH")
+#require(devtools)
+#has_devel()
+
+# y esperar un TRUE
+
+#--------------------------------
+
+
+hello <- function() {
+  print("Hello, world!")
+}
